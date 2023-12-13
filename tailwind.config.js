d7d33152c50f1eb05b49f672baa4a1ab729f4879
Kanneti Adithya <adithyakanneti05@gmail.com>
2023-12-13 12:40:39 +0530

    added frontend and backend

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          'custom-pink': '#ff49db',
+          'custom-purple': '#D0B3E6',
+        },
+        backgroundImage: theme => ({
+          'hero-pattern': "url('/img/hero-pattern.svg')", // replace with your image path
+        })
+      },
+    },
+    variants: {},
+    plugins: [],
+  };
+  
